77481dc923aaefff824b8c3c1a6b8a1e8f1ce768
Vernon <vernonht@gmail.com>
2018-04-22 23:55:04 +0800

    sails with MySQL DB

---
+++ api/controllers/TemperaturedataController.js
@@ -0,0 +1,13 @@
+/**
+ * TemperaturedataController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  getTemp: function() {
+          return 'temp';
+      },
+
+};
---
+++ api/models/Temperaturedata.js
@@ -0,0 +1,31 @@
+/**
+ * Temperaturedata.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    dateandtime: { type: 'string', required: true },
+    sensor: { type: 'string', required: true },
+    temperature: { type: 'number', columnType: 'FLOAT' },
+    humidity: { type: 'number' },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
